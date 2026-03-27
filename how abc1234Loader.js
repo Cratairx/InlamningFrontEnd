410602a2f72c5eb7963f0ea28f766b449ce20312
Cratairx <126484916+Cratairx@users.noreply.github.com>
2026-03-27 13:23:32 +0100

    fk commits

--- how abc1234Loader.js
+++
@@ -1,9 +0,0 @@
-[33md141035[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mDaniels-Branchs[m[33m, [m[1;31morigin/Daniels-Branchs[m[33m)[m Merge branch 'Daniels-Branchs' of https://github.com/Cratairx/InlamningFrontEnd into Daniels-Branchs
-[33m323ff62[m Merge branch 'main' of https://github.com/Cratairx/InlamningFrontEnd into Daniels-Branchs
-[33m9992f9d[m vg branch commit.
-[33m527e48e[m[33m ([m[1;31morigin/main[m[33m, [m[1;32mmain[m[33m)[m tog bort lite skräp
-[33m67b8270[m[33m ([m[1;31morigin/fixstylescomplete[m[33m, [m[1;32mfixstylescomplete[m[33m)[m Created styles.css from scratch, changed inside index.html and Loader.js to accomodate the new designlanguage, and make it work
-[33m06afcfa[m fix
-[33mbe4a5f2[m ny
-[33me58203b[m Kort fixade, formlär ska göras i Orderform, add to card button länken är redan addad.
-[33m7c5c0e6[m[33m ([m[1;31morigin/EmilsBranch[m[33m, [m[1;32mEmilsBranch[m[33m)[m Ändra namn tillbaka till index. Skapade footer och header fil och en loader för att ladda in dem. Lade till fåtal styles i styles.css för att fixa space issue med footer och header.
